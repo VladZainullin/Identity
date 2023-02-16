5f3c9a6be341400ced45827bfe00ce2ce098bd00
Vladislav Zainullin <mittoface02@mail.ru>
2023-02-16 18:33:01 +0300

    Добавляет метод расширения для будущей регистрации Application сервисов

---
+++ Identity.Application/ConfigureServices.cs
@@ -0,0 +1,14 @@
+using Microsoft.Extensions.DependencyInjection;
+using static System.Reflection.Assembly;
+
+namespace Identity.Application;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection ConfigureApplicationServices(this IServiceCollection services)
+    {
+        services.AddMediatR(c => c.RegisterServicesFromAssembly(GetExecutingAssembly()));
+
+        return services;
+    }
+}
