46f60e79fc9e2994170eb146cb645e3fa6d1d7f3
Vladislav Zainullin <mittoface02@mail.ru>
2023-02-18 12:58:03 +0300

    Добавляет и регистрирует AutoMapper

--- Identity.Application/ConfigureServices.cs
+++ Identity.Application/ConfigureServices.cs
@@ -8,6 +8,7 @@ public static class ConfigureServices
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddMediatR(c => c.RegisterServicesFromAssembly(GetExecutingAssembly()));
+        services.AddAutoMapper(GetExecutingAssembly());
 
         return services;
     }
