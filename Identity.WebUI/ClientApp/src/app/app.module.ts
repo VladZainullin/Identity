74dffda06ca4842d0b9d0421924a0964431a63f0
Vladislav Zainullin <mittoface02@mail.ru>
2023-03-12 15:11:55 +0300

    Добавялет форму регистрации

--- Identity.WebUI/ClientApp/src/app/app.module.ts
+++ Identity.WebUI/ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 
 import {AppComponent} from './app.component';
@@ -17,6 +17,7 @@ import { SignUpComponent } from './sign-up/sign-up.component';
     BrowserModule.withServerTransition({appId: 'ng-cli-universal'}),
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Identity.WebUI/ClientApp/src/app/sign-up/sign-up.component.ts
+++ Identity.WebUI/ClientApp/src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {CalendarDate} from "calendar-date";
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-sign-up',
@@ -6,12 +9,75 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private _httpClient: HttpClient,
+    @Inject('BASE_URL') private _baseUrl: string) {
   }
 
+  usernameControl: FormControl<string | null>;
+  username: string;
+  dateOfBirthControl: FormControl<CalendarDate | null>;
+  dateOfBirth: CalendarDate;
+  passwordControl: FormControl<string | null>;
+  password: string;
+  emailControl: FormControl<string | null>;
+  email: string;
 
+  onClick(): void {
 
+    let body: RegisterUserDto = {
+      username: this.username,
+      dateOfBirth: this.dateOfBirth,
+      email: this.email,
+      password: this.password
+    };
+
+    this._httpClient
+      .post(this._baseUrl + 'api/authorization/sign-up', body)
+      .subscribe({
+        next: (data: any) => console.log(data),
+        error: error => console.error(error)
+      });
+  }
+
+  ngOnInit() {
+    this.usernameControl = new FormControl<string | null>(null);
+    this.usernameControl.valueChanges
+      .subscribe(v => {
+        if (v != null) {
+          this.username = v
+        }
+      });
+
+    this.dateOfBirthControl = new FormControl<CalendarDate | null>(null);
+    this.dateOfBirthControl.valueChanges
+      .subscribe(v => {
+        if (v != null) {
+          this.dateOfBirth = v
+        }
+      });
+
+    this.emailControl = new FormControl<string | null>(null);
+    this.emailControl.valueChanges
+      .subscribe(v => {
+        if (v != null) {
+          this.email = v
+        }
+      });
+
+    this.passwordControl = new FormControl<string | null>(null);
+    this.passwordControl.valueChanges
+      .subscribe(v => {
+        if (v != null) {
+          this.password = v
+        }
+      });
+  }
+}
+
+export type RegisterUserDto = {
+  dateOfBirth: CalendarDate,
+  username: string,
+  email: string,
+  password: string
 }
